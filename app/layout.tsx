b670a419e60a069e8993922d86d3906321b5b42b
a10adotapp <mail@a10a.app>
2024-08-08 23:43:13 +0900

    viewport指定を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,14 @@
+import { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  viewport: {
+    initialScale: 1,
+    minimumScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  }
+};
 
 export default async function Layout({
   children,
