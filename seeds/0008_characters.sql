bc78d3a3170e7dfd726563a08f243f574b0bb755
a10adotapp <mail@a10a.app>
2024-08-08 17:04:26 +0900

    add character

---
+++ seeds/0008_characters.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+  `characters`
+  (`id`, `name`, `gif_url`, `image_url`, `profile_url`, `bust`, `waist`, `hip`, `height`)
+VALUES
+  ('29', '程普', '/image/character-101032.gif', '/image/character-101032.png', 'https://changokushi.com/characters/101032', '99', '62', '98', '188')
+;
